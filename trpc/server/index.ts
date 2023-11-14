1cc65e2f164c69b1c9da34e27f6a3a0287c6c41f
torutakakura <toosoccer.no2111@gmail.com>
2023-11-14 14:57:57 +0900

    made CRUD for blog

--- trpc/server/index.ts
+++ trpc/server/index.ts
@@ -1,11 +1,13 @@
 import { router } from "@/trpc/server/trpc"
 import { authRouter } from "@/trpc/server/routers/auth"
 import { userRouter } from "@/trpc/server/routers/user"
+import { postRouter } from "@/trpc/server/routers/post"
 
 // ルーターの作成
 export const appRouter = router({
   auth: authRouter,
   user: userRouter,
+  post: postRouter,
 })
 
 // ルーターの型定義
