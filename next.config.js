da218723bccec588b83b2286eb75a9443a6c2093
torutakakura <toosoccer.no2111@gmail.com>
2023-11-14 14:12:07 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      domains: ["lh3.googleusercontent.com", "res.cloudinary.com"],
+    },
+  }
+  
+  module.exports = nextConfig
+  
